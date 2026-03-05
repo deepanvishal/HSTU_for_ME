4a45eb75b0bfe58c340082a06337c3953c421a55
deepanvishal <deepanvishal@gmail.com>
2026-03-05 18:14:19 -0500

    Add future diagnosis list and update labels aggregation

--- label.sql
+++ label.sql
@@ -8,6 +8,7 @@ WITH visit_pairs AS (
         ,b.visit_date                                   AS future_visit_date
         ,b.specialty_codes                              AS future_specialty_codes
         ,b.provider_ids                                 AS future_provider_ids
+        ,b.dx_list                                      AS future_dx_list
         ,DATE_DIFF(b.visit_date, a.visit_date, DAY)     AS days_to_future_visit
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.sequence_table` a
     INNER JOIN `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.sequence_table` b
@@ -20,23 +21,26 @@ SELECT
     member_id
     ,visit_date
     ,visit_seq_num
-    ,ARRAY_AGG(DISTINCT specialty IGNORE NULLS)         AS specialties_180
-    ,ARRAY_AGG(DISTINCT CASE
-        WHEN days_to_future_visit <= 30
-        THEN specialty END IGNORE NULLS)                AS specialties_30
-    ,ARRAY_AGG(DISTINCT CASE
-        WHEN days_to_future_visit <= 60
-        THEN specialty END IGNORE NULLS)                AS specialties_60
-    ,ARRAY_AGG(DISTINCT CASE
-        WHEN days_to_future_visit <= 30
-        THEN provider END IGNORE NULLS)                 AS providers_30
-    ,ARRAY_AGG(DISTINCT CASE
-        WHEN days_to_future_visit <= 60
-        THEN provider END IGNORE NULLS)                 AS providers_60
-    ,ARRAY_AGG(DISTINCT provider IGNORE NULLS)          AS providers_180
+
+    -- specialty labels
+    ,ARRAY_AGG(DISTINCT CASE WHEN days_to_future_visit <= 30  THEN specialty END IGNORE NULLS) AS specialties_30
+    ,ARRAY_AGG(DISTINCT CASE WHEN days_to_future_visit <= 60  THEN specialty END IGNORE NULLS) AS specialties_60
+    ,ARRAY_AGG(DISTINCT specialty IGNORE NULLS)                                                AS specialties_180
+
+    -- provider labels
+    ,ARRAY_AGG(DISTINCT CASE WHEN days_to_future_visit <= 30  THEN provider END IGNORE NULLS) AS providers_30
+    ,ARRAY_AGG(DISTINCT CASE WHEN days_to_future_visit <= 60  THEN provider END IGNORE NULLS) AS providers_60
+    ,ARRAY_AGG(DISTINCT provider IGNORE NULLS)                                                AS providers_180
+
+    -- diagnosis labels
+    ,ARRAY_AGG(DISTINCT CASE WHEN days_to_future_visit <= 30  THEN dx END IGNORE NULLS)       AS dx_30
+    ,ARRAY_AGG(DISTINCT CASE WHEN days_to_future_visit <= 60  THEN dx END IGNORE NULLS)       AS dx_60
+    ,ARRAY_AGG(DISTINCT dx IGNORE NULLS)                                                      AS dx_180
+
 FROM visit_pairs
 CROSS JOIN UNNEST(future_specialty_codes)               AS specialty
 CROSS JOIN UNNEST(future_provider_ids)                  AS provider
+CROSS JOIN UNNEST(future_dx_list)                       AS dx
 GROUP BY
     member_id
     ,visit_date
