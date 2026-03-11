34fb139a0e603aab58a6b3f0034521d0b7ebb82c
deepanvishal <deepanvishal@gmail.com>
2026-03-11 17:02:36 -0400

    Update Data_prep_05_IQR.sql

--- Data_prep_05_IQR.sql
+++ Data_prep_05_IQR.sql
@@ -4,153 +4,154 @@ OPTIONS (labels=[("owner", "deepan_thulasi_aetna_com")])
 AS
 WITH bounds AS (
     SELECT 'dx_to_specialty_order1' AS table_name, member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)] AS q1
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)] AS q3
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)] AS log_q1
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)] AS log_q3
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_dx_to_specialty_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'dx_to_dx_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_dx_to_dx_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'dx_to_ccsr_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_dx_to_ccsr_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'specialty_to_specialty_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_specialty_to_specialty_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'specialty_to_dx_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_specialty_to_dx_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'specialty_to_ccsr_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_specialty_to_ccsr_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'ccsr_to_specialty_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_specialty_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'ccsr_to_dx_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_dx_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'ccsr_to_ccsr_order1', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_ccsr_order1`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'dx_to_specialty_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_dx_to_specialty_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'dx_to_dx_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_dx_to_dx_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'dx_to_ccsr_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_dx_to_ccsr_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'specialty_to_specialty_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_specialty_to_specialty_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'specialty_to_dx_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_specialty_to_dx_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'specialty_to_ccsr_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_specialty_to_ccsr_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'ccsr_to_specialty_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_specialty_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'ccsr_to_dx_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_dx_order2`
     GROUP BY member_segment
 
     UNION ALL
 
     SELECT 'ccsr_to_ccsr_order2', member_segment
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(1)]
-        ,APPROX_QUANTILES(transition_count, 4)[OFFSET(3)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(1)]
+        ,APPROX_QUANTILES(LOG(transition_count), 4)[OFFSET(3)]
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_ccsr_order2`
     GROUP BY member_segment
 )
 SELECT
     table_name
     ,member_segment
-    ,q1
-    ,q3
-    ,q3 - q1                                             AS iqr
-    ,GREATEST(q1 - 1.5 * (q3 - q1), 1)                  AS lower_fence
+    ,log_q1
+    ,log_q3
+    ,log_q3 - log_q1                                     AS log_iqr
+    ,log_q1 - 1.5 * (log_q3 - log_q1)                   AS log_lower_fence
+    ,CAST(EXP(log_q1 - 1.5 * (log_q3 - log_q1)) AS INT64) AS lower_fence
 FROM bounds
 ORDER BY table_name, member_segment
