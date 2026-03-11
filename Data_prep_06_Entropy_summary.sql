86bbec75b0661feeee7bbf316dfbdb748701d20b
deepanvishal <deepanvishal@gmail.com>
2026-03-11 17:36:13 -0400

    Update Data_prep_06_Entropy_summary.sql

--- Data_prep_06_Entropy_summary.sql
+++ Data_prep_06_Entropy_summary.sql
@@ -161,3 +161,46 @@ ccsr_to_specialty_o2 AS (
     FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_specialty_order2`
     WHERE transition_count >= 100 AND conditional_entropy IS NOT NULL
     GROUP BY member_segment
+),
+ccsr_to_dx_o2 AS (
+    SELECT 'ccsr_to_dx' AS combination, 2 AS markov_order, member_segment
+        ,SUM(transition_count * conditional_entropy) / SUM(transition_count)
+        ,APPROX_QUANTILES(conditional_entropy, 2)[OFFSET(1)]
+        ,SUM(transition_count)
+        ,COUNT(DISTINCT current_ccsr_v1)
+    FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_dx_order2`
+    WHERE transition_count >= 100 AND conditional_entropy IS NOT NULL
+    GROUP BY member_segment
+),
+ccsr_to_ccsr_o2 AS (
+    SELECT 'ccsr_to_ccsr' AS combination, 2 AS markov_order, member_segment
+        ,SUM(transition_count * conditional_entropy) / SUM(transition_count)
+        ,APPROX_QUANTILES(conditional_entropy, 2)[OFFSET(1)]
+        ,SUM(transition_count)
+        ,COUNT(DISTINCT current_ccsr_v1)
+    FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_gen_rec_f_ccsr_to_ccsr_order2`
+    WHERE transition_count >= 100 AND conditional_entropy IS NOT NULL
+    GROUP BY member_segment
+)
+SELECT combination, markov_order, member_segment
+    ,weighted_avg_entropy, median_entropy
+    ,total_transitions, unique_current_states
+FROM dx_to_specialty_o1
+UNION ALL SELECT * FROM dx_to_dx_o1
+UNION ALL SELECT * FROM dx_to_ccsr_o1
+UNION ALL SELECT * FROM specialty_to_specialty_o1
+UNION ALL SELECT * FROM specialty_to_dx_o1
+UNION ALL SELECT * FROM specialty_to_ccsr_o1
+UNION ALL SELECT * FROM ccsr_to_specialty_o1
+UNION ALL SELECT * FROM ccsr_to_dx_o1
+UNION ALL SELECT * FROM ccsr_to_ccsr_o1
+UNION ALL SELECT * FROM dx_to_specialty_o2
+UNION ALL SELECT * FROM dx_to_dx_o2
+UNION ALL SELECT * FROM dx_to_ccsr_o2
+UNION ALL SELECT * FROM specialty_to_specialty_o2
+UNION ALL SELECT * FROM specialty_to_dx_o2
+UNION ALL SELECT * FROM specialty_to_ccsr_o2
+UNION ALL SELECT * FROM ccsr_to_specialty_o2
+UNION ALL SELECT * FROM ccsr_to_dx_o2
+UNION ALL SELECT * FROM ccsr_to_ccsr_o2
+ORDER BY weighted_avg_entropy ASC, markov_order ASC, member_segment
