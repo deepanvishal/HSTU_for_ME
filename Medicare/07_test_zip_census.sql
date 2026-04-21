fbc9dd9c269391fe721e50b56bbb04cc7113352b
Deepan Vishal Thulasi Vel <deepanvishal@gmail.com>
2026-04-21 03:28:52 -0400

    Add queries for geography columns in zip_codes and counties
    Added queries to retrieve column names with geography data type from zip_codes and counties tables.

--- Medicare/07_test_zip_census.sql
+++ Medicare/07_test_zip_census.sql
@@ -13,3 +13,14 @@ SELECT
 FROM `bigquery-public-data.geo_us_boundaries.zip_codes` z
 CROSS JOIN florida_boundary f
 WHERE ST_INTERSECTS(z.zip_code_geom, f.state_geom)
+
+
+SELECT column_name 
+FROM `bigquery-public-data.geo_us_boundaries`.INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'zip_codes'
+AND data_type = 'GEOGRAPHY'
+
+SELECT column_name 
+FROM `bigquery-public-data.geo_us_boundaries`.INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'counties'
+AND data_type = 'GEOGRAPHY'
