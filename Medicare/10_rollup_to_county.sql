ba23560148a55d7f58c3d9694c10663103996879
Deepan Vishal Thulasi Vel <deepanvishal@gmail.com>
2026-04-21 04:54:46 -0400

    Update 10_rollup_to_county.sql

--- Medicare/10_rollup_to_county.sql
+++ Medicare/10_rollup_to_county.sql
@@ -5,3 +5,14 @@ SELECT
   COUNTIF(county_fips IS NULL)    AS null_county,
   COUNTIF(zip_lat IS NULL)        AS null_zip_lat
 FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_medicare_supply_demand_stg_providers`
+
+
+SELECT
+  COUNT(*) AS bene_zips
+FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_medicare_supply_demand_stg_beneficiaries`
+
+SELECT
+  COUNT(DISTINCT zip_cd) AS provider_zips,
+  COUNT(DISTINCT provider_id) AS providers,
+  COUNT(DISTINCT cms_specialty) AS specialties
+FROM `anbc-hcb-dev.provider_ds_netconf_data_hcb_dev.A870800_medicare_supply_demand_stg_providers`
